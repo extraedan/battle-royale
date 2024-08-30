0faba38ce006d7e32cf6c914d38e3b2aa477adbe
Edan <edanpchen@gmail.com>
2024-08-30 13:50:41 -0400

    Create database

--- app.py
+++ app.py
@@ -1,10 +1,20 @@
 from flask import Flask, render_template
+from sqlalchemy.orm import DeclarativeBase
+
 from forms import InputCharacter
 from flask_bootstrap import Bootstrap5
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
+# cCREATE FLASK SERVEr
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'dev'
 bootstrap = Bootstrap5(app)
+
+# CREATE DATABASE
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///battle_royale.db'
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
 
 @app.route('/')
