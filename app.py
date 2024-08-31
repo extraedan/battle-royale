60675403c9b2dc381b88cfdf82a63a10f09c8b8b
Edan <edanpchen@gmail.com>
2024-08-31 11:18:53 -0400

    fix: prevent form fields from retaining previously submitted data
    - Added redirect after successful form submission to clear form data
    - Ensured updated character names remain while avoiding unintended
      prefilled field

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
 from sqlalchemy.orm import DeclarativeBase
 
 from forms import InputCharacter
@@ -28,14 +28,20 @@ def home():
 
 @app.route('/create', methods=['GET', 'POST'])
 def choose_characters():
-    # //TODO Render each form separately
-    form = InputCharacter()
-    if request.method == 'POST' and form.validate_on_submit():
-        create_edit_character(form)
-    return render_template("choose_characters.html", form=form, characters=characters)
+    # creates four forms
+    forms = [InputCharacter() for _ in range(4)]
+
+    # if one of the forms is valid, create/edit character and break
+    if request.method == 'POST':
+        for form in forms:
+            if form.validate_on_submit():
+                create_edit_character(form)
+                return redirect(url_for('choose_characters'))
+    return render_template("choose_characters.html", forms=forms, characters=characters)
 
 
 def create_edit_character(form):
+    """Creates a new character object or changes the name of an existing character object"""
     index = int(form.slot.data)
     name = form.name.data
 
