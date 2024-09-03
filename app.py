0d96e214da05b76df4d5c0fe14b926b61b395f88
Edan <edanpchen@gmail.com>
2024-09-03 13:04:22 -0400

    Create input message to pass through API request to language model
    - Create temporary character pool to use for single or double character
      events
    - Pass one or two character objects into create_input_message function
    - Generate and return input message to send to language model by
      appending together attributes of character objects

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from forms import InputCharacter, NextEvent
 from flask_bootstrap import Bootstrap
 from characters import Character
 from events import events
+from anthropic_shenanagins import create_input_message
 import random
 
 # Create Flask Server
@@ -17,46 +18,33 @@ characters = [None, None, None, None]
 
 
 def generate_event():
-    """Returns a list of randomly generated event strings"""
-    current_events = []
-    for character in characters:
-        # 50% chance something happens to the character
-        if random.choice([True, False]):
-            char_a = character.name
+    print("You have entered generate event function")
+    # each round, make a list of living characters
+    events_to_display = []
+    character_pool = [character for character in characters if character.death == 0]
+    print(character_pool)
 
-            # chooses random event object
-            event = random.choice(events)
-            print(event)
-            print(event.text)
-            print(event.char_count)
+    # go through each character in the pool
+    print("You are starting to iterate")
+    for character in character_pool:
+        print("working step 1")
+        # define and remove from pool char_a
+        char_a = character
+        char_b = None # default value for char_b unless overridden
+        character_pool.remove(char_a)
 
-            # if single event
-            if event.char_count == 1:
+        # 50% chance another character is involved
+        # define and remove from pool char_b
+        if random.choice([True, False]) and len(character_pool) > 1:
+            char_b = random.choice(character_pool)
+            character_pool.remove(char_b)
 
-                # if death event, remove character object from pool
-                if event.death_num == 1:
-                    characters.remove(character)
-                # reformats the text
-                event = f"{event.text}".format(char_a=char_a)
-                current_events.append(event)
-
-            # if double event
-            elif event.char_count == 2:
-                # makes sure character b and character a are not the same
-                chars_pool = [char for char in characters if char.name != char_a]
-                char_b = random.choice(chars_pool)
-
-                # removes proper character object if death event
-                if event.death_num == 1:
-                    characters.remove(character)
-                elif event.death_num == 2:
-                    characters.remove(char_b)
-
-                event = f"{event.text}".format(char_a=char_a, char_b=char_b.name)
-                current_events.append(event)
+        # create input message
+        input_message = create_input_message(char_a, char_b)
+        print(input_message)
 
 
-    return current_events
+    return events_to_display
 
 @app.route('/game', methods=['GET', 'POST'])
 def play():
@@ -66,7 +54,8 @@ def play():
         # check if winner, if so render winner
         if len(characters) == 1:
             return render_template("winner.html", form=form, winner=characters[0].name)
-        events = generate_event()
+        print ("helloooo")
+        generate_event()
         return render_template("event.html", form=form, events=events)
     form = NextEvent()
     return render_template("game.html", characters=characters, form=form)
