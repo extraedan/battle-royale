73e38b626248fd16fb0adc41c825cb0c331d0ff9
Edan <edanpchen@gmail.com>
2024-09-07 14:27:47 -0400

    Limit character amount to 12

--- forms.py
+++ forms.py
@@ -1,7 +1,8 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, PasswordField, FileField, IntegerField
 from wtforms.fields.simple import HiddenField
-from wtforms.validators import DataRequired, URL
+from wtforms.validators import DataRequired, URL, NumberRange
+
 
 class InputCharacter(FlaskForm):
     name = StringField("", validators=[DataRequired()])
@@ -12,5 +13,11 @@ class NextEvent(FlaskForm):
     submit = SubmitField("Next event")
 
 class CharacterAmount(FlaskForm):
-    amount = IntegerField("Amount", validators=[DataRequired()])
+    amount = IntegerField(
+        "Amount",
+        validators=[
+            DataRequired(message="Please enter a number."),
+            NumberRange(min=1, max=12, message="The amount must be between 1 and 12.")
+        ]
+    )
     submit = SubmitField("Add amount")
